c11db82b6b18e6712e11006cb6c97b0233be398f
segalb <none>
2018-06-04 00:25:58 +0300

    tets

--- config/passport.js
+++ config/passport.js
@@ -203,19 +203,19 @@ module.exports = function(passport) {
 
   passport.use(new SamlStrategy(
   {
-   callbackUrl: 'https://statcart.herokuapp.com/auth/saml/callback',
+   callbackUrl: 'https://statcart.herokuapp.com/users/auth/saml/callback',
   //  path: '/login/callback',
     entryPoint: 'https://shibboleth.brandeis.edu/idp/profile/SAML2/Redirect/SSO',
-    issuer: 'statcart.herokuapp.com',
-    passReqToCallback: true,
+    issuer: 'statcart.herokuapp.com'
+    // passReqToCallback: true,
     // // Service Provider private key
     // decryptionPvk: fs.readFileSync(__dirname + '/cert/key.pem', 'utf8'),
     // // Service Provider Certificate
     // privateCert: fs.readFileSync(__dirname + '/cert/key.pem', 'utf8'),
     // // Identity Provider's public key
     // cert: fs.readFileSync(__dirname + '/cert/idp_cert1.pem', 'utf8'),
-    validateInResponseTo: false,
-      disableRequestedAuthnContext: true
+    // validateInResponseTo: false,
+    //   disableRequestedAuthnContext: true
   },
   function(profile, done) {
     findByEmail(profile.email, function(err, user) {
