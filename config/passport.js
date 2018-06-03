8c9c0cdcdc2cfd2ff1df675f3972d67d6898d706
segalb <none>
2018-06-04 00:05:07 +0300

    test12

--- config/passport.js
+++ config/passport.js
@@ -208,12 +208,12 @@ module.exports = function(passport) {
     entryPoint: 'https://shibboleth.brandeis.edu/idp/profile/SAML2/Redirect/SSO',
     issuer: 'statcart.herokuapp.com',
     passReqToCallback: true,
-    // Service Provider private key
-    decryptionPvk: fs.readFileSync(__dirname + '/cert/key.pem', 'utf8'),
-    // Service Provider Certificate
-    privateCert: fs.readFileSync(__dirname + '/cert/key.pem', 'utf8'),
-    // Identity Provider's public key
-    cert: fs.readFileSync(__dirname + '/cert/idp_cert1.pem', 'utf8'),
+    // // Service Provider private key
+    // decryptionPvk: fs.readFileSync(__dirname + '/cert/key.pem', 'utf8'),
+    // // Service Provider Certificate
+    // privateCert: fs.readFileSync(__dirname + '/cert/key.pem', 'utf8'),
+    // // Identity Provider's public key
+    // cert: fs.readFileSync(__dirname + '/cert/idp_cert1.pem', 'utf8'),
     validateInResponseTo: false,
       disableRequestedAuthnContext: true
   },
