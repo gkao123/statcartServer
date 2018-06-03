f40aed445dec130bdb609611f32a2f1c853d6965
segalb <none>
2018-06-03 15:08:01 +0300

    tesT

--- config/passport.js
+++ config/passport.js
@@ -213,7 +213,7 @@ module.exports = function(passport) {
     // Service Provider Certificate
     privateCert: fs.readFileSync(__dirname + '/cert/key.pem', 'utf8'),
     // Identity Provider's public key
-    cert: fs.readFileSync(__dirname + '/cert/idp_cert1.pem', 'utf8'),
+    cert: fs.readFileSync(__dirname + '/cert/idp_cert.pem', 'utf8'),
     validateInResponseTo: false,
       disableRequestedAuthnContext: true
   },
